608d892037c5f24b4a5895c0fdfef450e722a2f2
Ariane <ariane.fairlie@gmail.com>
2020-08-27 14:00:56 -0400

    search partially working. bug: dynamic update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+// interface search {
+//   results: movie[],
+//   term: string
+// }
+
+// interface movie {
+//   title: string,
+//   year: number,
+//   nominated: boolean
+// }
+
 const App: React.FC = () => {
-  let [state, setState] = useState<any>()
+  let [search, setSearch] = useState<any>({results: [], term: 'ram'})
 
   useEffect(() => {
-    fetch('http://www.omdbapi.com/?i=tt3896198&apikey=538adb24&t=apocalypse+now')
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=538adb24&s=${search.term}&type=movie`)
     .then(res => res.json())
-    .then(res => setState(res.Title))
-  }, [])
+    .then(res => {
+      setSearch({...search, results: [...res.Search.map((movie: { Title: any; }) => movie.Title)]})
+      console.log(res)
+    })
+  }, [search.term])
 
   return (
     <div>
-      {state}
+      <input type='text' value={search.term} onChange={e => setSearch({...search, term: e.target.value})}></input>
+      <ul>
+        {search.results.map((r: React.ReactNode, index: number) => {
+          return <li key={index} >{r}</li>
+        })}
+      </ul>
     </div>
   );
 }
