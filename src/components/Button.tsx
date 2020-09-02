a816afaf2004edefd8b6cf467da3002e8976db0f
Ariane <ariane.fairlie@gmail.com>
2020-09-01 22:04:37 -0400

    clear search button implemented

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,7 @@ interface props {
   disabled?: boolean;
   primary?: boolean;
   cancel?: boolean;
+  clearSearch?: boolean;
   onClick: any;
 }
 
@@ -50,11 +51,25 @@ const CancelButton = styled(StyledButton)`
   }
 `
 
-export const Button: React.FC<props> = ({children, primary, cancel, disabled, onClick}) => {
+const ClearSearch = styled(CancelButton)`
+  border: none;
+  color: ${({theme}) => (theme.colors.secondary)};
+  width: ${({theme}) => (theme.fontSize.lg)};
+  align-self: center;
+  margin: 0;
+
+  @media (max-width: 779px) {
+    font-size: ${({theme}) => (theme.fontSize.xs)};
+    width: ${({theme}) => (theme.fontSize.md)};
+  }
+`
+
+export const Button: React.FC<props> = ({children, primary, cancel, clearSearch, disabled, onClick}) => {
   return (
     <>
       {primary && <PrimaryButton disabled={disabled} onClick={onClick}>{children}</PrimaryButton>}
       {cancel && <CancelButton disabled={disabled} onClick={onClick}>{children}</CancelButton>}
+      {clearSearch && <ClearSearch disabled={disabled} onClick={onClick}>{children}</ClearSearch>}
     </>
   )
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect } from 'react';
 import { useDebounce } from '../hooks/useDebounce';
 import styled from 'styled-components'
+import {Button} from './Button'
 
 interface props {
   onSearch: (term: string) => void,
@@ -8,14 +9,19 @@ interface props {
   setValue: React.Dispatch<React.SetStateAction<string>>
 }
 
+// margin: ${({theme}) => (`${theme.spacing.sm} ${theme.spacing.md} ${theme.spacing.md} 0`)};
+// padding: ${({theme}) => (theme.spacing.xs)};
+
+
 const Input = styled.input`
   color: ${({theme}) => (theme.colors.text)};
   font-size: ${({theme}) => (theme.fontSize.md)};
-  margin: ${({theme}) => (`${theme.spacing.sm} 0 ${theme.spacing.md}`)};
-  padding: ${({theme}) => (theme.spacing.xs)};
   padding-left: ${({theme}) => (theme.spacing.sm)};
+  margin-right: ${({theme}) => (theme.spacing.sm)};
   border-radius: ${({theme}) => (theme.borderRadius.curve)};
   border: none;
+  flex: 3;
+  align-self: center;
   
   box-shadow: inset ${({theme}) => {
     const xs = theme.spacing.xs
@@ -30,6 +36,11 @@ const Input = styled.input`
     font-size: ${({theme}) => (theme.fontSize.sm)};
     padding-left: ${({theme}) => (theme.spacing.md)};
   }
+`
+const FlexBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: ${({theme}) => (`${theme.spacing.sm} 0 ${theme.spacing.md}`)}
 `
 
 export const SearchBar: React.FC<props> = ({ onSearch, value, setValue }) => {
@@ -48,12 +59,15 @@ export const SearchBar: React.FC<props> = ({ onSearch, value, setValue }) => {
 
   return (
     <>
-      <Input 
-        type='text'
-        value={value}
-        onChange={e => setValue(e.target.value)}
-        placeholder='search a movie'
-      />
+      <FlexBox>
+        <Input 
+          type='text'
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          placeholder='search a movie'
+        />
+        <Button clearSearch onClick={() => setValue('')}>clear search</Button>
+      </FlexBox>
     </>
   )
 }
