b291adac0d22504fb6624373806d3b8475b6cbce
Ariane <ariane.fairlie@gmail.com>
2020-09-18 14:24:15 -0400

    changed footer description to wip

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ export function Footer() {
   return (
     <div>
       <Cred>
-        <span>an app by Ariane Fairlie </span>
+        <span>a work in progress by Ariane Fairlie </span>
         <a href="https://www.github.com/afairlie/" target='_blank' rel="noopener noreferrer">
           <Icon whileHover={{ scale: 1.5, y: -5}} src="https://img.icons8.com/nolan/64/github.png" alt='github icon' />
         </a>
