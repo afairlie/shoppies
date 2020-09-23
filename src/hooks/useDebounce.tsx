0e96cb8275a9f364f52524f93ec9fefb55424d35
Ariane <ariane.fairlie@gmail.com>
2020-09-23 11:20:12 -0400

    cleanup

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -1,15 +1,15 @@
 import {useState, useEffect} from 'react'
 
-type Hook = (term: string, ms: number) => string;
+type Hook = (value: string, ms: number) => string;
 
-export const useDebounce: Hook = (term, ms) => {
+export const useDebounce: Hook = (value, ms) => {
   const [debounced, setDebounced] = useState('');
 
   useEffect(() => {
-    const timeout = setTimeout(() => setDebounced(term), ms);
+    const timeout = setTimeout(() => setDebounced(value), ms);
     // cleanup to prevent repeated calls
     return () => clearTimeout(timeout);
-  }, [term, ms])
+  }, [value, ms])
 
   return debounced
 }
